2e57b0b9139304c4815037f19b79311c149e6771
Le Ma <le_ma_@mail.ru>
2017-09-24 22:29:22 +0300

    Code refactoring

--- db_core.py
+++ db_core.py
@@ -1,16 +1,19 @@
+"""
+DB core functions
+"""
 import logging
 import time
 import psycopg2
 import psycopg2.errorcodes
 
-import tittles as t
+import tittles as t # pylint: disable-msg=E0401
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format="%(levelname)s: %(asctime)s %(module)s:%(lineno)d(%(funcName)s) %(message)s", level=logging.DEBUG)
-
-
-_log = logging.getLogger(__name__)
+    logging.basicConfig(
+        format="%(levelname)s: %(module)s:%(lineno)d(%(funcName)s) %(message)s",
+        level=logging.DEBUG)
+_log = logging.getLogger(__name__)  # pylint: disable-msg=C0103
 
 
 RECONNECT_ATTEMPTS = 0
@@ -18,194 +21,257 @@ RECONNECT_PAUSE = 500
 PG_DEFAULT_PORT = "5432"
 
 
-def debug_statement(**kwargs):
-    if kwargs.get("debug") == "statement" and not kwargs.get("no_logging"): return True
-
-
-def apply_statement(**kwargs):
-    if kwargs.get("apply") == "statement": return True
+def _debug_stmt(**kwargs):
+    if kwargs.get("debug") == "statement" and not kwargs.get("no_logging"):
+        return True
 
 
 class Db():
+    """DB connection class with core functions"""
 
     def __init__(self, parms, **kwargs):
         self.__conn = None
         self.__user = None
         self.__pass = None
-        self.__db = None
+        self.__dbname = None
         self.__host = None
         self.__port = None
         self.__set_parms(parms, **kwargs)
 
+    def __str__(self):
+        return "%s @ %s : %s (connected=%s, id=%s)" % (
+            self.__user == self.__dbname and self.__user or "%s(%s)" % (self.__user, self.__dbname),
+            self.__host, self.__port, self.is_connected(), hex(id(self)), )
+
     def __set_parms(self, parms, **kwargs):
         changed_ = 0
 
-        def __set_if_changed(val, dic, key, default):
+        def __set_if_changed(current_val, dic, key, default=None):
             nonlocal changed_
-            changed_val_ = dic.get(key, default)
-            if changed_val_ != val: changed_ += 1; return changed_val_
-            return val
+            changed_val_ = t.nvl(dic.get(key, default), default)
+            if changed_val_ and changed_val_ != current_val:
+                changed_ += 1
+                return changed_val_
+            return current_val
 
         # USER
-        self.__user = __set_if_changed(self.__user, parms, "USER", None)
-        if not self.__user: raise t.ConfigError("USER is not defined")
+        self.__user = __set_if_changed(self.__user, parms, "USER")
+        if not self.__user:
+            raise t.ConfigError("USER is not defined")
         # PASSWORD
-        self.__pass = __set_if_changed(self.__pass, parms, "PASSWORD", None)
-        if not self.__pass: raise t.ConfigError("PASSWORD is not defined")
+        self.__pass = __set_if_changed(self.__pass, parms, "PASSWORD")
+        if not self.__pass:
+            raise t.ConfigError("PASSWORD is not defined")
         # DB
-        self.__db = __set_if_changed(self.__db, parms, "DATABASE", kwargs.get("database_equals_user") and self.__user or "postgres")
+        self.__dbname = __set_if_changed(
+            self.__dbname, parms, "DATABASE",
+            kwargs.get("database_equals_user") and self.__user or "postgres")
         # HOST
         self.__host = __set_if_changed(self.__host, parms, "HOST", "localhost")
         # PORT
         self.__port = __set_if_changed(self.__port, parms, "PORT", PG_DEFAULT_PORT)
 
-        return changed_ and True or False
+        return True if changed_ else False
 
     def set_parms(self, parms, **kwargs):
-        # Set parameters
+        """Set connection parameters"""
         if self.__set_parms(parms, **kwargs):
             # Close connection if any parameter updated
-            _log.debug("closing db connection")
             self.close(**kwargs)
 
-    def is_connected(self):
-        if self.__conn and self.__conn.closed == 0: return True
-        return False
-
-    def __str__(self):
-        return "%s @ %s : %s (connected=%s)" % (
-            self.__user == self.__db and self.__user or "%s(%s)" % (self.__user, self.__db),
-            self.__host, self.__port, self.is_connected(), )
-
-    def close(self, **kwargs):
-        if self.__conn:
-            if debug_statement(**kwargs): _log.debug("Closing connection")
-            self.__conn.close()
-            self.__conn = None
-        return self
-
-    def __get_conn(self, **kwargs):
-        if not self.__conn: self.connect(**kwargs)
-        return self.__conn
-
     def encoding(self):
+        """Get DB encoding"""
         return self.__get_conn().encoding
 
     def connect(self, **kwargs):
-        if debug_statement(**kwargs):
-            _log.debug("Connecting to %s, reopen=%s." % (self, kwargs.get("reopen")))
-        if self.__conn:
+        """Create new database connection"""
+        if _debug_stmt(**kwargs):
+            _log.debug("Connect to %s (reopen=%s)", self, kwargs.get("reopen", False))
+        if self.is_connected():
+            # If reopen - close connection
             if kwargs.get("reopen"):
                 self.close(**kwargs)
-            else:
-                if debug_statement(**kwargs):
-                    _log.debug("Already connected, reopen=%s." % kwargs.get("reopen"))
-                return self.__conn # Nothing to do - already connected
+            # Nothing to do - already connected
+            else: return self.__conn
         try:
             self.__conn = psycopg2.connect(
                 "".join([
                     "host=", self.__host,
-                    " dbname=", self.__db,
+                    " dbname=", self.__dbname,
                     " user=", self.__user,
                     " password=", self.__pass,
                     " port=", self.__port,
                 ])
             )
-            if debug_statement(**kwargs): _log.debug("Connected to %s" % self)
-        except psycopg2.Error as e_:
-            raise t.DbConnectError("Cannot connect to %s: %s" % (self, e_))
+        except psycopg2.Error as e_:    # pylint: disable-msg=C0103
+            raise t.DbConnectError("Can't connect to %s: %s" % (self, e_))
         return self.__conn
 
+    def __get_conn(self, **kwargs):
+        if not self.__conn:
+            self.connect(**kwargs)
+        return self.__conn
+
+    def is_connected(self):
+        """Return DB connection status"""
+        if self.__conn and self.__conn.closed == 0:
+            return True
+        return False
+
+    def close(self):
+        """Close connection"""
+        if self.__conn:
+            self.__conn.close()
+            self.__conn = None
+        return self
+
     def commit(self):
-        if self.__conn: self.__conn.commit()
+        """Commit"""
+        if self.__conn:
+            self.__conn.commit()
         return self
 
     def rollback(self):
-        if self.__conn: self.__conn.rollback()
+        """Rollback"""
+        if self.__conn:
+            self.__conn.rollback()
         return self
 
     def cursor(self, **kwargs):
+        """Create DB cursor"""
         return self.__get_conn(**kwargs).cursor()
 
-    def cursor_close(self, cursor):
-        if cursor: cursor.close()
+    def cursor_close(self, cursor): # pylint: disable-msg=R0201
+        """Close cursor"""
+        if cursor:
+            cursor.close()
 
-    def __exec_w_reconn(self, stmt, args=None, **kwargs):
+    def __select(self, stmt, args, **kwargs):
+        csr_ = self.cursor(**kwargs)
+        csr_.execute(stmt, args)
+        return csr_
+
+    def __execute(self, stmt, args, **kwargs):
+        csr_ = None
+        try:
+            csr_ = self.cursor(**kwargs)
+            csr_.execute("begin")
+            csr_.execute("savepoint svp_" + hex(id(csr_)))
+            csr_.execute(stmt, args)
+            return csr_.rowcount
+        except Exception:
+            if csr_:
+                csr_.execute("rollback to svp_" + hex(id(csr_)))
+            raise
+        finally:
+            self.cursor_close(csr_)
+
+    def __exec_reconn_wrapper(self, stmt, args, exec_func, **kwargs):
+        # Retrieve number of attempts and puse between them
         attempts_ = kwargs.get("reconnect_attempts", RECONNECT_ATTEMPTS)
         pause_ = kwargs.get("reconnect_pause", RECONNECT_PAUSE)
-        stmt_ = t.lotovts(stmt)
-        csr_ = None
+
+        # Compose statement
+        stmt_ = t.lovts(stmt)
+        if _debug_stmt(**kwargs):
+            _log.debug(
+                "STMT [%s] ARGS %s (attempts=%s, pause=%s, id=%s)",
+                stmt_, args, attempts_, pause_, hex(id(self)))
+
+        # Do execute several times
         while attempts_ >= 0:
             try:
-                if debug_statement(**kwargs):
-                    _log.debug("STMT [%s] ARGS %s (attempts=%s, pause=%s)" % (stmt_, args, attempts_, pause_))
-                csr_ = self.__get_conn(**kwargs).cursor()
-                csr_.execute(stmt_, args)
-                attempts_ = -1 # Stop reconnecting on success
-            except psycopg2.Error as e_:
-                # TODO - test exception handling when closing connection
-                # Check ignore error codes
-                def __ignore_errs(e_, **kwargs):
-                    ignore_errs_ = kwargs.get("ignore_errs")
-                    if ignore_errs_ and (e_.pgcode in ignore_errs_): return True
-                if e_.pgcode and __ignore_errs(e_, **kwargs):
-                    _log.warning("Can't execute (ignored) STMT [%s] ARGS %s (connected=%s, pgcode=%s, class=%s): %s" % (
-                        stmt_, args, self.is_connected(), e_.pgcode, e_.__class__.__name__, e_))
-                    break
+                return exec_func(stmt_, args, **kwargs)
+
+            except psycopg2.Error as e_:    # pylint: disable-msg=C0103
+
+                # Check ignore error function
+                def __ignore_errs(e_, **kwargs):    # pylint: disable-msg=C0103
+                    if e_.pgcode:
+                        ignore_errs_ = kwargs.get("ignore_errs")
+                        if ignore_errs_ and (e_.pgcode in ignore_errs_):
+                            return True
+
+                # In case of error in ignore list - do nothing, simply return
+                if __ignore_errs(e_, **kwargs):
+                    if not kwargs.get("no_logging"):
+                        _log.warning(
+                            ("Can't execute (ignored) STMT [%s] ARGS %s"
+                             " (connected=%s, pgcode=%s, class=%s, id=%s): %s"),
+                            stmt_, args, self.is_connected(), e_.pgcode,
+                            e_.__class__.__name__, hex(id(self)), e_)
+                    return
+
                 # If error not related to closed connection
                 # or number of attemts exhausted - raise exception
-                if not self.__get_conn().closed or attempts_ <= 0 or e_.pgcode in ("25P02", ):
-                    raise t.DbExecuteError("Can't execute STMT [%s] ARGS %s (connected=%s, pgcode=%s, class=%s): %s" % (
-                        stmt_, args, self.is_connected(), e_.pgcode, e_.__class__.__name__, e_))
+                if not self.__get_conn().closed or attempts_ <= 0:
+                    raise t.DbExecuteError((
+                        "Can't execute STMT [%s] ARGS %s"
+                        " (connected=%s, pgcode=%s, class=%s, id=%s): %s") % (
+                            stmt_, args, self.is_connected(),
+                            e_.pgcode, e_.__class__.__name__, hex(id(self)), e_))
+
                 # ... else log warning and keep trying execution
                 if not kwargs.get("no_logging"):
-                    _log.warning("Can't execute STMT [%s] ARGS %s (connected=%s, pgcode=%s, class=%s): %s" % (
-                        stmt_, args, self.is_connected(), e_.pgcode, e_.__class__.__name__, e_))
-                self.cursor_close(csr_)
+                    _log.warning(
+                        ("Can't execute STMT [%s] ARGS %s"
+                         " (connected=%s, pgcode=%s, class=%s, id=%s): %s"),
+                        stmt_, args, self.is_connected(),
+                        e_.pgcode, e_.__class__.__name__, hex(id(self)), e_)
+
                 time.sleep(pause_ / 1000.0)
+                # do reconnect
                 self.connect(reopen=True, **kwargs)
                 attempts_ -= 1
-        return csr_
 
     def select(self, stmt, args=None, **kwargs):
-        csr_ = self.__exec_w_reconn(stmt, args, **kwargs)
-        return csr_
+        """Execute select statement and return DB cursor"""
+        return self.__exec_reconn_wrapper(stmt, args, self.__select, **kwargs)
 
     def select_one(self, stmt, args=None, **kwargs):
+        """Select one DB record"""
         csr_ = None
         try:
             csr_ = self.select(stmt, args, **kwargs)
             return csr_.fetchone()
-        finally: csr_ and csr_.close()
+        finally:
+            if csr_:
+                csr_.close()
 
     def select_all(self, stmt, args=None, **kwargs):
+        """Select all DB records"""
         csr_ = None
         try:
             csr_ = self.select(stmt, args, **kwargs)
             return csr_.fetchall()
-        finally: csr_ and csr_.close()
+        finally:
+            if csr_:
+                csr_.close()
 
     def execute(self, stmt, args=None, **kwargs):
-        csr_ = None
-        rowcount_ = -1
-        try:
-            csr_ = self.__exec_w_reconn(stmt, args, **kwargs)
-            rowcount_ = csr_.rowcount
-            if kwargs.get("commit") == "statement": self.commit()
-        finally:
-            self.cursor_close(csr_)
+        """Execute DB statement"""
+        rowcount_ = self.__exec_reconn_wrapper(stmt, args, self.__execute, **kwargs)
+        if kwargs.get("commit") == "statement":
+            self.commit(**kwargs)
         return rowcount_
 
     def execute_batch(self, batch, **kwargs):
+        """Execute batch of DB statements"""
         rowcount_ = -1
-        for s_ in batch:
+        for s_ in batch:    # pylint: disable-msg=C0103
             rowcount_ += self.execute(s_, **kwargs)
-        if kwargs.get("commit") == "batch": self.commit()
+        if kwargs.get("commit") == "batch":
+            self.commit(**kwargs)
         return rowcount_
+
+    def __del__(self):
+        self.rollback()
+        self.close()
 
 
 def test():
-    import time
+    """Test function"""
+    # import time
 
     db_parms_ = {
         "USER": "ffba_backup",
@@ -215,21 +281,31 @@ def test():
     conn_ = Db(db_parms_, database_equals_user=True, debug="statement")
     conn_.connect(debug="statement")
 
-    _log.debug("setting parms without changes...")
-    conn_.set_parms(db_parms_, database_equals_user=True)
+    conn_.execute("create table xx (i integer, t text)")
+    conn_.execute("insert into xx (i, t) values (1, 'text 1')")
+    conn_.execute("insert into xx_ (i, t) values (2, 'text 2')", ignore_errs=("42P01",))
+    conn_.execute("insert into xx (i, t) values (3, 'text 3')")
+    for r_ in conn_.select_all("select * from xx"): # pylint: disable-msg=C0103
+        _log.debug("R: %s", (r_, ))
 
-    db_parms_ = {
-        "USER": "ffba_170908",
-        "PASSWORD": "f__",
-    }
-
-    _log.debug("setting changed parms...")
-    conn_.set_parms(db_parms_)
-    conn_.connect(debug="statement")
-
-    time.sleep(20) # kill DB connection during the pause
-
-    conn_.execute("DROP TABLE NON_EXISTENT_TABLE", ignore_errs=(psycopg2.errorcodes.UNDEFINED_TABLE), reconnect_attempts=1, debug="statement")
+    # _log.debug("setting parms without changes...")
+    # conn_.set_parms(db_parms_, database_equals_user=True)
+    #
+    # db_parms_ = {
+    #     "USER": "ffba_170908",
+    #     "PASSWORD": "f__",
+    # }
+    #
+    # _log.debug("setting changed parms...")
+    # conn_.set_parms(db_parms_)
+    # conn_.connect(debug="statement")
+    #
+    # time.sleep(20) # kill DB connection during the pause
+    #
+    # conn_.execute(
+        # "DROP TABLE NON_EXISTENT_TABLE",
+        # ignore_errs=(psycopg2.errorcodes.UNDEFINED_TABLE),
+        # reconnect_attempts=1, debug="statement")
 
     # csr_ = None
     # try:
